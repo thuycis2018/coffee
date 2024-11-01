d8bed328ac2e4cf2472d84d86e0e30459ae47bda
TL <thuycis@gmail.com>
2024-10-31 20:53:13 -0700

    refactor(style): update 404 page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -5,10 +5,15 @@ const Error = () => {
   const error = useRouteError();
   if (error.status === 404) {
     return (
-      <div>
-        <img src={img} alt='page not found' />
-        <p>Page Not Found</p>
-        <Link to='/'>Home</Link>
+      <div className='min-h-full grid items-center justify-center pt-10'>
+        <img src={img} alt='not found' />
+        <h3 className='text-2xl text-center pt-10'>Ohh!</h3>
+        <p className='text-2xl text-center pt-10'>
+          We cannot seem to find page you are looking for
+        </p>
+        <Link to='/' className='text-2xl text-center capitalize pt-10'>
+          back home
+        </Link>
       </div>
     );
   }
